ff9b426b07aabf10a8fe6a231385d40abe9bb5ed
Alex K <kjerte@gmail.com>
2016-10-09 15:51:40 +0200

    added board, cell, player and tests

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,5 +1,9 @@
 require "tictactoe/version"
 
-module Tictactoe
+module TicTacToe
   # Your code goes here...
 end
+
+require_relative "./tictactoe/cell.rb"
+require_relative "./tictactoe/player.rb"
+require_relative "./tictactoe/board.rb"
